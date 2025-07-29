bdd19f639176851409198cf548aa0e1cec2f9f0b
Mehar Klair <meharklair123@gmail.com>
2025-07-29 16:01:47 -0600

    set up langchain integration with paper retrival

--- Data Retrival/retrieve_papers.py
+++ Data Retrival/retrieve_papers.py
@@ -1,31 +1,31 @@
 from elasticsearch import Elasticsearch, helpers
 from langchain_elasticsearch import ElasticsearchRetriever
 
+es_url = "https://my-elasticsearch-project-ee9962.es.us-west-2.aws.elastic.cloud:443"
+es_api_key="U0xrWE5aZ0JQcFBpODk4akFvclU6WWFFVW80RUZjWDR4Tzd0T2VhZ1BRUQ=="
+
 
 client = Elasticsearch(
     "https://my-elasticsearch-project-ee9962.es.us-west-2.aws.elastic.cloud:443",
     api_key="U0xrWE5aZ0JQcFBpODk4akFvclU6WWFFVW80RUZjWDR4Tzd0T2VhZ1BRUQ=="
 )
 
-def retrieve_data(query):
-    retriever_object = {
-        "standard": {
-            "query": {
-                "multi_match": {
-                    "query": query,
-                    "fields": [
-                        "title"
-                    ]
-                }
-            }
-        }
+def bm25_query(search_query: str) -> dict:
+    return {
+        "query": {
+            "match": {
+                text_field: search_query,
+            },
+        },
     }
 
-    search_response = client.search(
-        index="search-papers",
-        retriever=retriever_object,
-    )
-    return search_response['hits']['hits']
-
-        
-retrieve_data("artificial")
+text_field = "abstract"
+retriever = ElasticsearchRetriever.from_es_params(
+    index_name="search-papers",
+    body_func=bm25_query,
+    content_field=text_field,
+    url=es_url,
+    api_key=es_api_key
+)
+    
+print(retriever.invoke("Artificial"))
