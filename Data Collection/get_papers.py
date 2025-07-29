00757315918b334702f59ccdfc36ef457b3356ec
Mehar Klair <meharklair123@gmail.com>
2025-07-29 15:59:42 -0600

    flattened the lists within the dictionary

--- Data Collection/get_papers.py
+++ Data Collection/get_papers.py
@@ -5,50 +5,65 @@ from io import BytesIO
 from elasticsearch import Elasticsearch, helpers
 
 elastic_cloud_address = "https://my-elasticsearch-project-ee9962.es.us-west-2.aws.elastic.cloud:443",
- 
+index_name = "search-papers"
 
 client = Elasticsearch(
     elastic_cloud_address,
     api_key="SUlhVUw1Z0J5X3JBUlFBZGR1ZjI6VlZ3R2pGaDRYMmxRYjNSLWF5S2FoUQ=="
 )
-index_name = "search-papers"
 
-
-mappings = {
-    "properties": {
-        "id":        {"type": "keyword"},
-        "created":   {"type": "date"},   # default: yyyy-MM-dd
-        "updated":   {"type": "date"},
-        "authors":   {"type": "text"},  # or "text" if full name search desired
-        "author":    {"type": "text"},  # or "text"
-        "keyname":   {"type": "keyword"},
-        "forenames": {"type": "text"},
-        "title":     {"type": "text"},
-        "categories":{"type": "keyword"},
-        "comments":  {"type": "text"},
-        "report-no": {"type": "keyword"},
-        "abstract":  {"type": "text"},
+def create_mappings():
+    mappings = {
+        "properties": {
+            "id":        {"type": "keyword"},
+            "created":   {"type": "date"},   # default: yyyy-MM-dd
+            "updated":   {"type": "date"},
+            "authors":   {"type": "list"},  # or "text" if full name search desired
+            "author":    {"type": "list"},  # or "text"
+            "keyname":   {"type": "keyword"},
+            "forenames": {"type": "text"},
+            "title":     {"type": "text"},
+            "categories":{"type": "keyword"},
+            "comments":  {"type": "text"},
+            "report-no": {"type": "keyword"},
+            "abstract":  {"type": "text"},
+        }
     }
-}
-mapping_response = client.indices.put_mapping(index=index_name, body=mappings)
-print(mapping_response)
+    mapping_response = client.indices.put_mapping(index=index_name, body=mappings)
+    print(mapping_response)
+
+
+def flatten_lists_in_dict(d, join_str="\n"):
+    """
+    Given a dictionary `d`, convert any value that is a list into a string by 
+    joining the list elements with `join_str`.
+    Returns a new dictionary with updated values.
+    """
+    new_dict = {}
+    for k, v in d.items():
+        if isinstance(v, list):
+            # Convert list to string, joining elements
+            # Optionally convert non-str elements to str before joining
+            str_items = [str(item) for item in v]
+            new_dict[k] = join_str.join(str_items)
+        else:
+            new_dict[k] = v
+    return new_dict
+
 
 sickle = Sickle('http://export.arxiv.org/oai2')
 records = sickle.ListRecords(metadataPrefix='arXiv', set='cs')
-
-record_upload = []
 i = 0
+record_upload = []
 for record in records:
-    if i == 3:
+    if i == 10:
+        bulk_response = helpers.bulk(client, record_upload, index=index_name)
+        print(bulk_response)
         break
-    metadata = record.metadata
-    arxiv_id = metadata["id"][0]
+    metadata = flatten_lists_in_dict(record.metadata)
     #pdf_url = f"https://arxiv.org/pdf/{arxiv_id}.pdf"
     record_upload.append(metadata)
     i += 1
     
-    bulk_response = helpers.bulk(client, record_upload, index=index_name)
-    print(bulk_response)
     
     
-  
--- Data Retrival/retrieve_papers.py
+++ Data Retrival/retrieve_papers.py
@@ -1,32 +1,31 @@
 from elasticsearch import Elasticsearch, helpers
 from langchain_elasticsearch import ElasticsearchRetriever
 
-elastic_cloud_address = "https://my-elasticsearch-project-ee9962.es.us-west-2.aws.elastic.cloud:443",
-api_key = "U0xrWE5aZ0JQcFBpODk4akFvclU6WWFFVW80RUZjWDR4Tzd0T2VhZ1BRUQ=="
-
 
 client = Elasticsearch(
     "https://my-elasticsearch-project-ee9962.es.us-west-2.aws.elastic.cloud:443",
     api_key="U0xrWE5aZ0JQcFBpODk4akFvclU6WWFFVW80RUZjWDR4Tzd0T2VhZ1BRUQ=="
 )
 
-retriever_object = {
-    "standard": {
-        "query": {
-            "multi_match": {
-                "query": "Artificial",
-                "fields": [
-                    "title"
-                ]
+def retrieve_data(query):
+    retriever_object = {
+        "standard": {
+            "query": {
+                "multi_match": {
+                    "query": query,
+                    "fields": [
+                        "title"
+                    ]
+                }
             }
         }
     }
-}
 
-search_response = client.search(
-    index="search-papers",
-    retriever=retriever_object,
-)
-for item in search_response['hits']['hits']:
-    print(item)
-    
+    search_response = client.search(
+        index="search-papers",
+        retriever=retriever_object,
+    )
+    return search_response['hits']['hits']
+
+        
+retrieve_data("artificial")
