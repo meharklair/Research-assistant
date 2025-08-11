f71d221e061f76fbf81d6f5638a490ce8a8a9877
meharklair <meharklair123@gmail.com>
2025-08-11 16:10:30 -0600

    started working on the search API endpoint

--- LLM_Interface/construct_prompt.py
+++ LLM_Interface/construct_prompt.py
@@ -1,7 +1,5 @@
 from langchain.prompts import PromptTemplate
-from langchain_core.prompts import ChatPromptTemplate
 from Data_Retrival.retrieve_papers import retrive_papers
-import ollama
 
 def create_prompt(user_query):
     # Example prompt template
@@ -25,18 +23,15 @@ def create_prompt(user_query):
         template=user_prompt,
         input_variables=["context", "question"]
     )
+    
     retrieved_docs = retrive_papers(user_query)
-    #print(retrieved_docs)
+
     # Concatenate or summarize retrieved passages
     context = "\n\n".join(doc.page_content for doc in retrieved_docs)
     user_prompt = prompt.format(context=context, question=user_query)
-
-    response = ollama.chat(
-        model='llama2:7b',
-        messages=[
+    messages =[
             {'role': 'system', 'content': system_prompt},
             {'role': 'user', 'content': user_prompt},
         ]
-    )
 
-    return response['message']['content']
+    return messages
--- main.py
+++ main.py
@@ -1,3 +1,35 @@
 from LLM_Interface.construct_prompt import create_prompt
+from fastapi import FastAPI
+from LLM_Interface.construct_prompt import create_prompt
+from LLM_Interface.query_model import query_model
 
-print(create_prompt("Neural Networks in an Artificial Chemistry"))
+import re
+app = FastAPI()
+
+def normalize_query(user_query):
+    phrases_to_remove = [
+        r"can you find me", r"please find", r"show me", r"look up", r"find papers on"
+    ]
+    normalized = user_query.lower()
+    for phrase in phrases_to_remove:
+        normalized = re.sub(phrase, "", normalized)
+    return normalized.strip()
+
+@app.get("/search")
+async def search_papers(query):
+    # Search for papers with matching title in your Elasticsearch index (e.g., "papers")
+    normalized_query = normalize_query(query)
+    prompt = create_prompt(normalized_query)
+    return query_model('llama2:7b', prompt)
+
+def main():
+    prompt = create_prompt("Neural Networks in an Artificial Chemistry")
+    print(prompt)
+    print(query_model('llama2:7b',prompt))
+    
+    
+    
+    
+if __name__ == "__main__":
+    main()
+    
