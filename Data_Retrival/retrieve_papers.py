d4dbc099a8923757076f1540fbcbb6bf46b6ed5d
Mehar Klair <meharklair123@gmail.com>
2025-08-20 22:10:58 -0600

    partially fixed paper retrival

--- Data_Retrival/retrieve_papers.py
+++ Data_Retrival/retrieve_papers.py
@@ -1,9 +1,10 @@
 from elasticsearch import Elasticsearch, helpers
-from langchain_elasticsearch import ElasticsearchRetriever
+from langchain.vectorstores.elasticsearch import ElasticsearchStore
+from langchain.embeddings.openai import OpenAIEmbeddings
 
 es_url = "https://05aa1e0d47ee450da2fc6a10f39fc911.us-central1.gcp.cloud.es.io:443"
 es_api_key="SFJKWWhwZ0JxQjU3LVNLT1d4NmY6XzlHSXE2REV3MUM3eEhaQzQyUW80Zw=="
-
+embedding = OpenAIEmbeddings()
 
 
 def retrive_papers(query):
@@ -12,24 +13,20 @@ def retrive_papers(query):
             "query": {
                 "multi_match": {
                     "query": search_query,
-                    "fields": [
-                        "abstract",
-                        "fornames",
-                        "keyname",
-                        "title"
-                    ]
-                },
+                    "fields": ["abstract", "fornames", "keyname", "title"]
+                }
             },
-        "min_score": 2.0  
+            "_source": ["abstract", "fornames", "keyname", "title"],
+            "min_score": 2.0
         }
 
-    text_field = "abstract"
-    retriever = ElasticsearchRetriever.from_es_params(
-        index_name="search-sep4",
-        body_func=bm25_query,
-        content_field=text_field,
-        url=es_url,
-        api_key=es_api_key
-    )
-    retrived_docs = retriever.invoke(query)
+    
+
+    retriever = ElasticsearchStore(
+    index_name="search-sep4",
+    embedding=embedding,
+    url=es_url,
+    api_key=es_api_key
+    ),
+    retrived_docs = retriever.similarity_search(query, k=5, custom_query=bm25_query)
     return retrived_docs if retrived_docs else None
