76c976d8f9ab1282d19e893ee3b0e33b2cba03f5
Mehar Klair <meharklair123@gmail.com>
2025-08-21 12:57:22 -0600

    fixed a bug in the data retrieval

--- Data_Retrival/retrieve_papers.py
+++ Data_Retrival/retrieve_papers.py
@@ -4,7 +4,17 @@ from langchain_elasticsearch import ElasticsearchRetriever
 es_url = "https://05aa1e0d47ee450da2fc6a10f39fc911.us-central1.gcp.cloud.es.io:443"
 es_api_key="SFJKWWhwZ0JxQjU3LVNLT1d4NmY6XzlHSXE2REV3MUM3eEhaQzQyUW80Zw=="
 
-
+def extract_information(retrieved_docs):
+    # flatten dictionary to get results
+    retrieved_docs = retrieved_docs.body["hits"]["hits"]
+    
+    # creates a list of dicts with only the needed info
+    document_list = []
+    for doc in retrieved_docs:
+        document_list.append(doc["_source"])
+    print(document_list)
+    return document_list
+       
 
 def retrive_papers(query):
 
@@ -12,26 +22,20 @@ def retrive_papers(query):
         es_url,
         api_key=es_api_key
     )
-    retrived_docs = client.search(
+    retrieved_docs = client.search(
     index="search-sep4",
     body={
-        "_source": ["title", "abstract", "keywords", "content"],
+        "_source": ["title", "abstract", "forenames", "keyname"],
         "query": {
             "multi_match": {
-                "query": "neural networks image recognition",
+                "query": query,
                 "fields": ["title", "abstract", "forenames", "keyname"]
             },
         },
     "min_score": 2.0
     }
     ) 
-    # flatten dictionary to get results
-    retrived_docs = retrived_docs.body["hits"]["hits"]
+    retrieved_docs = extract_information(retrieved_docs)
+    return retrieved_docs if retrieved_docs else None
 
-    # extracts the dictionary in the list
-    merged_dict = {}
-    for d in retrived_docs:
-        merged_dict.update(d)
-    retrived_docs = merged_dict["_source"]
-    print(retrived_docs)
-    return retrived_docs if retrived_docs else None
+
